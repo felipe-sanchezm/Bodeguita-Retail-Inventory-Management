a17b7edc11816f2431989e0c0c7abbd6d597b181
Felipe Sanchez <contacto.fsanchezm@gmail.com>
2026-04-09 21:39:15 -0600

    Actualizacion Catalogo Productos
    Se actualiza el script de llenado con datos de prueba reales.
    Se cambia el método del módulo de Productos.

--- sql/02_Llenado de información.sql
+++ sql/02_Llenado de información.sql
@@ -39,6 +39,14 @@ INSERT INTO Departamento (Nombre_Departamento) VALUES
 ('Juguetería Niños'), ('Juguetería Niñas'), ('Juguetería Bebés'), ('Coleccionables'), 
 ('Papelería'), ('Dulcería'), ('Dulcería Granel'), ('Dulcería Checkout'), ('Playa'), ('Navideño');
 
+-- 6. Estatus de Transito
+INSERT INTO Estatus_Transito (Descripcion_Estatus_Transito) VALUES
+('Pendiente Validar'),
+('En Camino'),
+('Recibido en Tienda'),
+('Cancelado'),
+('Demorado');
+
 
 --========================
 --INSERTAR FUERZA LABORAL
@@ -88,68 +96,127 @@ INSERT INTO Proveedor (Razon_Social_Proveedor, RFC_Proveedor) VALUES
 ('Abarrotes y Dulces del Centro', 'ADC951210LKP');
 
 -- 2. PRODUCTOS 
--- INSERTAR 50 PRODUCTOS CON LÓGICA DE Departamento
-INSERT INTO Productos (SKU_Producto, Material_Producto, Descripcion_Producto, ID_Marca, ID_Proveedor, ID_Departamento)
+INSERT INTO Productos (SKU_Producto, Material_Producto, Descripcion_Producto, Costo_Unitario, ID_Marca, ID_Proveedor, ID_Departamento)
 VALUES 
--- BLOQUE ROPA (ID_Departamento vinculados a nombres de Ropa)
-('7501000000000001', '5001', 'Bra Copa B con varilla y encaje floral - Corsetería', 1, 3, (SELECT TOP 1 ID_Departamento FROM Departamento WHERE Nombre_Departamento='Corsetería y Lencería')),
-('7501000000000002', '5001', 'Pack 3 Pantaletas Niña algodón - Niñas Interior', 4, 3, (SELECT TOP 1 ID_Departamento FROM Departamento WHERE Nombre_Departamento='Niñas Interior')),
-('7501000000000003', '5020', 'Jeans Dama Juvenil Corte Skinny Azul - Dama Juvenil', 12, 3, (SELECT TOP 1 ID_Departamento FROM Departamento WHERE Nombre_Departamento='Dama Juvenil')),
-('7501000000000004', '5020', 'Pantalón Caballero Mezclilla Recto - Caballero Juvenil', 12, 3, (SELECT TOP 1 ID_Departamento FROM Departamento WHERE Nombre_Departamento='Caballero Juvenil')),
-('7501000000000005', '5001', 'Mameluco Bebé Algodón Estampado - Bebés Meses', 4, 3, (SELECT TOP 1 ID_Departamento FROM Departamento WHERE Nombre_Departamento='Bebés Meses')),
-('7501000000000006', '5044', 'Sudadera Deportiva con Gorro - Deportivo', 11, 3, (SELECT TOP 1 ID_Departamento FROM Departamento WHERE Nombre_Departamento='Deportivo')),
-('7501000000000007', '5001', 'Camiseta Interior Niño Blanca - Niños Interior', 4, 3, (SELECT TOP 1 ID_Departamento FROM Departamento WHERE Nombre_Departamento='Niños Interior')),
-('7501000000000008', '5020', 'Short Juvenil Mezclilla - Teens 14-18', 12, 3, (SELECT TOP 1 ID_Departamento FROM Departamento WHERE Nombre_Departamento='Teens 14-18')),
-('7501000000000009', '5088', 'Vestido Dama Estampado Flores - Dama', 12, 3, (SELECT TOP 1 ID_Departamento FROM Departamento WHERE Nombre_Departamento='Dama')),
-('7501000000000010', '5001', 'Calceta Escolar Blanca Par - Escolar', 6, 3, (SELECT TOP 1 ID_Departamento FROM Departamento WHERE Nombre_Departamento='Escolar')),
+-- DEPT 1: CORSETERÍA Y LENCERÍA
+('7501000000000001', 10000001, 'Bustier Encaje Negro V1', 550.00, 1, 3, 1),
+('7501000000000002', 10000002, 'Bata de Seda Rosa', 890.00, 1, 3, 1),
+('7501000000000003', 10000003, 'Pack 3 Panties Algodon', 249.00, 1, 3, 1),
+('7501000000000004', 10000004, 'Bra Strapless Basico', 420.00, 1, 3, 1),
+('7501000000000005', 10000005, 'Corset Reductor Pro', 680.00, 1, 3, 1),
+('7501000000000006', 10000006, 'Pijama Satinada 2 pz', 720.00, 1, 3, 1),
+('7501000000000007', 10000007, 'Faja Alta Compresion', 950.00, 1, 3, 1),
+('7501000000000008', 10000008, 'Baby Doll Encaje Rojo', 480.00, 1, 3, 1),
+('7501000000000009', 10000009, 'Body de Malla Negro', 310.00, 1, 3, 1),
+('7501000000000010', 10000010, 'Liguero con Medias Red', 199.00, 1, 3, 1),
 
--- BLOQUE JUGUETERÍA Y TEMPORADA (Bodega Externa 2 - A004)
-('8401000000000011', '100255', 'Figura de Acción Marvel Legends - Juguetería Niños', 1, 2, (SELECT TOP 1 ID_Departamento FROM Departamento WHERE Nombre_Departamento='Juguetería Niños')),
-('8401000000000012', '100255', 'Muñeca Barbie Fashionista - Juguetería Niñas', 2, 2, (SELECT TOP 1 ID_Departamento FROM Departamento WHERE Nombre_Departamento='Juguetería Niñas')),
-('8401000000000013', '100990', 'Set Lego Star Wars 500pzas - Juguetería Niños', 3, 2, (SELECT TOP 1 ID_Departamento FROM Departamento WHERE Nombre_Departamento='Juguetería Niños')),
-('8401000000000014', '100440', 'Peluche Suave Personaje Disney - Juguetería Bebés', 4, 2, (SELECT TOP 1 ID_Departamento FROM Departamento WHERE Nombre_Departamento='Juguetería Bebés')),
-('8401000000000015', '200110', 'Alberca Inflable Familiar - Playa', 4, 2, (SELECT TOP 1 ID_Departamento FROM Departamento WHERE Nombre_Departamento='Playa')),
-('8401000000000016', '300550', 'Árbol Navidad Artificial 1.90m - Navideño', 4, 2, (SELECT TOP 1 ID_Departamento FROM Departamento WHERE Nombre_Departamento='Navideño')),
-('8401000000000017', '100255', 'Carro Control Remoto Todoterreno - Juguetería Niños', 1, 2, (SELECT TOP 1 ID_Departamento FROM Departamento WHERE Nombre_Departamento='Juguetería Niños')),
-('8401000000000018', '100255', 'Cocina de Juguete con Accesorios - Juguetería Niñas', 2, 2, (SELECT TOP 1 ID_Departamento FROM Departamento WHERE Nombre_Departamento='Juguetería Niñas')),
-('8401000000000019', '100990', 'Rompecabezas 1000 piezas Paisaje - Coleccionables', 4, 2, (SELECT TOP 1 ID_Departamento FROM Departamento WHERE Nombre_Departamento='Coleccionables')),
-('8401000000000020', '200110', 'Salvavidas Circular Infantil - Playa', 4, 2, (SELECT TOP 1 ID_Departamento FROM Departamento WHERE Nombre_Departamento='Playa')),
+-- DEPT 7: DAMA
+('7507000000000001', 70000001, 'Vestido Noche Corte Sirena', 1850.00, 2, 3, 7),
+('7507000000000002', 70000002, 'Blazer Ejecutivo Marino', 1200.00, 2, 3, 7),
+('7507000000000003', 70000003, 'Jeans Levanta Pompa Mezclilla', 650.00, 2, 3, 7),
+('7507000000000004', 70000004, 'Blusa Gasa Floral', 380.00, 2, 3, 7),
+('7507000000000005', 70000005, 'Falda Midi Plisada', 520.00, 2, 3, 7),
+('7507000000000006', 70000006, 'Sueter Punto Oversize', 450.00, 2, 3, 7),
+('7507000000000007', 70000007, 'Abrigo de Panio Invierno', 2300.00, 2, 3, 7),
+('7507000000000008', 70000008, 'Top Satinado Tirantes', 290.00, 2, 3, 7),
+('7507000000000009', 70000009, 'Pantalon Vestir Recto', 780.00, 2, 3, 7),
+('7507000000000010', 70000010, 'Chaleco Acolchado Dama', 1100.00, 2, 3, 7),
 
--- BLOQUE VARIEDADES / HOGAR / PAPELERÍA (Bodega Externa 1 - A003)
-('9101000000000021', '8005544', 'Sartén Antiadherente 24cm - Hogar', 8, 1, (SELECT TOP 1 ID_Departamento FROM Departamento WHERE Nombre_Departamento='Hogar')),
-('9101000000000022', '8007722', 'Plancha de Vapor Cerámica - Enseres Menores', 9, 1, (SELECT TOP 1 ID_Departamento FROM Departamento WHERE Nombre_Departamento='Enseres Menores')),
-('9101000000000023', '40011', 'Cuaderno Profesional Raya 100H - Papelería', 5, 1, (SELECT TOP 1 ID_Departamento FROM Departamento WHERE Nombre_Departamento='Papelería')),
-('9101000000000024', '40022', 'Caja de Colores 24 piezas - Papelería', 13, 1, (SELECT TOP 1 ID_Departamento FROM Departamento WHERE Nombre_Departamento='Papelería')),
-('9101000000000025', '90033', 'Toalla Baño Algodón Premium - Blancos', 12, 1, (SELECT TOP 1 ID_Departamento FROM Departamento WHERE Nombre_Departamento='Blancos')),
-('9101000000000026', '80011', 'Licuadora 10 Velocidades - Enseres Menores', 9, 1, (SELECT TOP 1 ID_Departamento FROM Departamento WHERE Nombre_Departamento='Enseres Menores')),
-('9101000000000027', '70055', 'Set de Platos Cerámica 4pzas - Hogar', 8, 1, (SELECT TOP 1 ID_Departamento FROM Departamento WHERE Nombre_Departamento='Hogar')),
-('9101000000000028', '60011', 'Cama para Mascota Mediana - Mascotas', 4, 1, (SELECT TOP 1 ID_Departamento FROM Departamento WHERE Nombre_Departamento='Mascotas')),
-('9101000000000029', '40011', 'Calculadora Científica - Papelería', 16, 1, (SELECT TOP 1 ID_Departamento FROM Departamento WHERE Nombre_Departamento='Papelería')),
-('9101000000000030', '90055', 'Edredón Matrimonial Estampado - Blancos', 12, 1, (SELECT TOP 1 ID_Departamento FROM Departamento WHERE Nombre_Departamento='Blancos')),
+-- DEPT 15: CABALLEROS INTERIOR
+('7515000000000001', 15000001, 'Boxer Ajustado Algodon', 180.00, 3, 3, 15),
+('7515000000000002', 15000002, 'Pack 3 Camisetas Cuello V', 450.00, 3, 3, 15),
+('7515000000000003', 15000003, 'Bermuda para Dormir Gris', 320.00, 3, 3, 15),
+('7515000000000004', 15000004, 'Bata de Banio Microfibra', 650.00, 3, 3, 15),
+('7515000000000005', 15000005, 'Calcetin Ejecutivo Negro', 85.00, 3, 3, 15),
+('7515000000000006', 15000006, 'Boxer Brief Deportivo', 220.00, 3, 3, 15),
+('7515000000000007', 15000007, 'Tirantes Ajustables Elasticos', 290.00, 3, 3, 15),
+('7515000000000008', 15000008, 'Pijama Franela Cuadros', 580.00, 3, 3, 15),
+('7515000000000009', 15000009, 'Calzoncillo Clasico Blanco', 120.00, 3, 3, 15),
+('7515000000000010', 15000010, 'Calceta Termica Invierno', 150.00, 3, 3, 15),
 
--- BLOQUE PERFUMERÍA / CUIDADO PERSONAL (Bodega Externa 1)
-('6301000000000031', '12345', 'Shampoo Control Caspa 1L - Tintes y Shampoos', 16, 1, (SELECT TOP 1 ID_Departamento FROM Departamento WHERE Nombre_Departamento='Tintes y Shampoos')),
-('6301000000000032', '12345', 'Jabón Líquido Corporal 500ml - Cuidado Personal', 7, 1, (SELECT TOP 1 ID_Departamento FROM Departamento WHERE Nombre_Departamento='Cuidado Personal')),
-('6301000000000033', '55667', 'Crema Dental Triple Acción - Belleza', 15, 1, (SELECT TOP 1 ID_Departamento FROM Departamento WHERE Nombre_Departamento='Belleza')),
-('6301000000000034', '88990', 'Secadora de Cabello Profesional - Eléctrico Cuidado Personal', 9, 1, (SELECT TOP 1 ID_Departamento FROM Departamento WHERE Nombre_Departamento='Eléctrico Cuidado Personal')),
-('6301000000000035', '11223', 'Desodorante Roll-on Dama - Cuidado Personal', 15, 1, (SELECT TOP 1 ID_Departamento FROM Departamento WHERE Nombre_Departamento='Cuidado Personal')),
-('6301000000000036', '12345', 'Tinte para Cabello Tono 5.0 - Tintes y Shampoos', 16, 1, (SELECT TOP 1 ID_Departamento FROM Departamento WHERE Nombre_Departamento='Tintes y Shampoos')),
-('6301000000000037', '55667', 'Labial Larga Duración Rojo - Belleza', 15, 1, (SELECT TOP 1 ID_Departamento FROM Departamento WHERE Nombre_Departamento='Belleza')),
-('6301000000000038', '11223', 'Paquete Rastrillos 4pzas - Checkout', 15, 1, (SELECT TOP 1 ID_Departamento FROM Departamento WHERE Nombre_Departamento='Checkout')),
-('6301000000000039', '11223', 'Gel Antibacterial 250ml - Checkout', 7, 1, (SELECT TOP 1 ID_Departamento FROM Departamento WHERE Nombre_Departamento='Checkout')),
-('6301000000000040', '12345', 'Acondicionador Brillo Intenso - Tintes y Shampoos', 16, 1, (SELECT TOP 1 ID_Departamento FROM Departamento WHERE Nombre_Departamento='Tintes y Shampoos')),
+-- DEPT 26: ENSERES MENORES
+('7526000000000001', 26000001, 'Freidora de Aire 4.5L', 1650.00, 4, 1, 26),
+('7526000000000002', 26000002, 'Cafetera Goteo 12 Tazas', 590.00, 4, 1, 26),
+('7526000000000003', 26000003, 'Tostadora Acero Inox', 420.00, 4, 1, 26),
+('7526000000000004', 26000004, 'Batidora Mano 5 Vel', 350.00, 4, 1, 26),
+('7526000000000005', 26000005, 'Exprimidor Citricos Elec', 280.00, 4, 1, 26),
+('7526000000000006', 26000006, 'Olla Electrica Lenta', 1100.00, 4, 1, 26),
+('7526000000000007', 26000007, 'Waflera Antiadherente', 499.00, 4, 1, 26),
+('7526000000000008', 26000008, 'Parrilla Elec 2 Quemadores', 850.00, 4, 1, 26),
+('7526000000000009', 26000009, 'Hervidor Agua Elec 1.7L', 390.00, 4, 1, 26),
+('7526000000000010', 26000010, 'Sandwichera Prensa Grill', 410.00, 4, 1, 26),
 
--- BLOQUE DULCERÍA / BEBÉS (Bodega Externa 1 / Externa 2)
-('4201000000000041', '101010', 'Bolsa Dulce Surtido 1kg - Dulcería', 16, 4, (SELECT TOP 1 ID_Departamento FROM Departamento WHERE Nombre_Departamento='Dulcería')),
-('4201000000000042', '101010', 'Gomitas Mango Enchilado - Dulcería Granel', 16, 4, (SELECT TOP 1 ID_Departamento FROM Departamento WHERE Nombre_Departamento='Dulcería Granel')),
-('4201000000000043', '101010', 'Paletas Tutsi Pop Bolsa - Dulcería Checkout', 16, 4, (SELECT TOP 1 ID_Departamento FROM Departamento WHERE Nombre_Departamento='Dulcería Checkout')),
-('4201000000000044', '202020', 'Pañales Etapa 3 40pzas - Cuidado del bebé', 4, 1, (SELECT TOP 1 ID_Departamento FROM Departamento WHERE Nombre_Departamento='Cuidado del bebé')),
-('4201000000000045', '202020', 'Toallitas Húmedas Pack 3 - Cuidado del bebé', 4, 1, (SELECT TOP 1 ID_Departamento FROM Departamento WHERE Nombre_Departamento='Cuidado del bebé')),
-('4201000000000046', '202020', 'Biberón Anti-cólicos - Cuidado del bebé', 4, 1, (SELECT TOP 1 ID_Departamento FROM Departamento WHERE Nombre_Departamento='Cuidado del bebé')),
-('4201000000000047', '101010', 'Malvaviscos Cubiertos Chocolate - Dulcería', 16, 4, (SELECT TOP 1 ID_Departamento FROM Departamento WHERE Nombre_Departamento='Dulcería')),
-('4201000000000048', '101010', 'Chocolate Carlos V Caja - Dulcería Checkout', 16, 4, (SELECT TOP 1 ID_Departamento FROM Departamento WHERE Nombre_Departamento='Dulcería Checkout')),
-('4201000000000049', '5001', 'Manta Recibidora Bebé - Bebés Blancos', 4, 3, (SELECT TOP 1 ID_Departamento FROM Departamento WHERE Nombre_Departamento='Bebés Blancos')),
-('4201000000000050', '202020', 'Mordedera de Goma Fría - Cuidado del bebé', 4, 1, (SELECT TOP 1 ID_Departamento FROM Departamento WHERE Nombre_Departamento='Cuidado del bebé'));
+-- DEPT 28: HOGAR
+('7528000000000001', 28000001, 'Juego Sartenes Antiadherente', 1200.00, 5, 1, 28),
+('7528000000000002', 28000002, 'Set Cuchillos Profesionales', 850.00, 5, 1, 28),
+('7528000000000003', 28000003, 'Cesta Ropa Sucia Tejida', 350.00, 5, 1, 28),
+('7528000000000004', 28000004, 'Espejo Pared Marco Madera', 1450.00, 5, 1, 28),
+('7528000000000005', 28000005, 'Lampara Escritorio LED', 420.00, 5, 1, 28),
+('7528000000000006', 28000006, 'Cuadro Decorativo Moderno', 600.00, 5, 1, 28),
+('7528000000000007', 28000007, 'Alfombra Sala 1.5x2mts', 2100.00, 5, 1, 28),
+('7528000000000008', 28000008, 'Reloj Pared Silencioso', 380.00, 5, 1, 28),
+('7528000000000009', 28000009, 'Organizador Zapatos 10 Niv', 550.00, 5, 1, 28),
+('7528000000000010', 28000010, 'Cortina Blackout Gris', 720.00, 5, 1, 28),
+
+-- DEPT 31: ELECTRÓNICA
+('7531000000000001', 31000001, 'Smart TV 55 Pulgadas 4K', 8900.00, 6, 1, 31),
+('7531000000000002', 31000002, 'Laptop 15.6 Pulgadas 8GB', 12500.00, 6, 1, 31),
+('7531000000000003', 31000003, 'Smartphone 128GB Unlocked', 4500.00, 6, 1, 31),
+('7531000000000004', 31000004, 'Consola Videojuegos 512GB', 7200.00, 6, 1, 31),
+('7531000000000005', 31000005, 'Bocina Alexa Echo Dot', 1100.00, 6, 1, 31),
+('7531000000000006', 31000006, 'Tablet 10 Pulgadas Estudiante', 3200.00, 6, 1, 31),
+('7531000000000007', 31000007, 'Camara Seguridad WiFi', 850.00, 6, 1, 31),
+('7531000000000008', 31000008, 'Mouse Inalambrico Basico', 250.00, 6, 1, 31),
+('7531000000000009', 31000009, 'Teclado Mecanico RGB', 1400.00, 6, 1, 31),
+('7531000000000010', 31000010, 'Cable HDMI 4K 2mts', 180.00, 6, 1, 31),
+
+-- DEPT 33: ZAPATERÍA
+('7533000000000001', 33000001, 'Tenis Correr Hombre', 1600.00, 7, 1, 33),
+('7533000000000002', 33000002, 'Bota Industrial Casquillo', 1250.00, 7, 1, 33),
+('7533000000000003', 33000003, 'Zapato Vestir Piel', 1400.00, 7, 1, 33),
+('7533000000000004', 33000004, 'Sandalias Playa Casual', 320.00, 7, 1, 33),
+('7533000000000005', 33000005, 'Zapatilla Tacon Dama', 890.00, 7, 1, 33),
+('7533000000000006', 33000006, 'Mocasin Gamuza Joven', 750.00, 7, 1, 33),
+('7533000000000007', 33000007, 'Tenis Lona Clasicos', 550.00, 7, 1, 33),
+('7533000000000008', 33000008, 'Bota Lluvia Infantil', 420.00, 7, 1, 33),
+('7533000000000009', 33000009, 'Pantuflas Descanso Felpa', 190.00, 7, 1, 33),
+('7533000000000010', 33000010, 'Botin Casual Gamuza', 1150.00, 7, 1, 33),
+
+-- DEPT 37: JUGUETERÍA NIÑOS
+('7537000000000001', 37000001, 'Set Construccion Bloques', 1200.00, 8, 2, 37),
+('7537000000000002', 37000002, 'Pista Carreras Triple Loop', 1850.00, 8, 2, 37),
+('7537000000000003', 37000003, 'Figura Accion Superheroe', 350.00, 8, 2, 37),
+('7537000000000004', 37000004, 'Lanza Dardos Espuma', 620.00, 8, 2, 37),
+('7537000000000005', 37000005, 'Coche Control Remoto Offroad', 950.00, 8, 2, 37),
+('7537000000000006', 37000006, 'Dinosaurio T-Rex Sonidos', 480.00, 8, 2, 37),
+('7537000000000007', 37000007, 'Telescopio Astronomo Junior', 1100.00, 8, 2, 37),
+('7537000000000008', 37000008, 'Set Quimica Experimentos', 750.00, 8, 2, 37),
+('7537000000000009', 37000009, 'Pelota Futbol Profesional', 450.00, 8, 2, 37),
+('7537000000000010', 37000010, 'Robot Inteligente Programable', 2100.00, 8, 2, 37),
+
+-- DEPT 41: PAPELERÍA
+('7541000000000001', 41000001, 'Caja Hojas Blanca 5000pz', 1100.00, 9, 4, 41),
+('7541000000000002', 41000002, 'Set Marcadores Arte 24pz', 480.00, 9, 4, 41),
+('7541000000000003', 41000003, 'Libreta Profesional Pasta Dura', 180.00, 9, 4, 41),
+('7541000000000004', 41000004, 'Calculadora Cientifica Pro', 650.00, 9, 4, 41),
+('7541000000000005', 41000005, 'Mochila Escolar Ergonomica', 950.00, 9, 4, 41),
+('7541000000000006', 41000006, 'Set Geometria Metal', 120.00, 9, 4, 41),
+('7541000000000007', 41000007, 'Agenda Ejecutiva 2026', 350.00, 9, 4, 41),
+('7541000000000008', 41000008, 'Engrapadora Uso Pesado', 420.00, 9, 4, 41),
+('7541000000000009', 41000009, 'Pack Boligrafos Tinta Gel', 250.00, 9, 4, 41),
+('7541000000000010', 41000010, 'Organizador Escritorio Malla', 290.00, 9, 4, 41),
+
+-- DEPT 42: DULCERÍA
+('7542000000000001', 42000001, 'Caja Chocolate Surtido 1kg', 650.00, 10, 4, 42),
+('7542000000000002', 42000002, 'Bolsa Gomitas Frutales 500g', 120.00, 10, 4, 42),
+('7542000000000003', 42000003, 'Pack 24 Paletas Caramelo', 180.00, 10, 4, 42),
+('7542000000000004', 42000004, 'Estuche Dulce Regional 2kg', 890.00, 10, 4, 42),
+('7542000000000005', 42000005, 'Malvaviscos Cubiertos Choco', 240.00, 10, 4, 42),
+('7542000000000006', 42000006, 'Caja Mazapan Grande 30pz', 210.00, 10, 4, 42),
+('7542000000000007', 42000007, 'Canasta Navidenia Dulces', 1450.00, 10, 4, 42),
+('7542000000000008', 42000008, 'Dulce Tamarindo Picante 1kg', 320.00, 10, 4, 42),
+('7542000000000009', 42000009, 'Set Regalo Chocolates Finos', 1100.00, 10, 4, 42),
+('7542000000000010', 42000010, 'Bolsa Dulce Checkout Mix', 55.00, 10, 4, 42);
 
 -- 1. ENTRADA MASIVA INICIAL (Carga de Proveedor a Bodega)
 INSERT INTO Movimientos (ID_Producto, Cantidad, ID_Almacen_Origen, ID_Almacen_Destino, ID_Usuario, ID_Empleado, FUM, ID_Tipo_Movimiento)
